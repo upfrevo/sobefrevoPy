15cabfcc708ed39a40dcc27e4edfc28dd9942d74
jpataide <jpataide@gmail.com>
2017-11-21 13:11:11 -0200

    atualizando arquivos e adicionando beacons

--- LED.py
+++ LED.py
@@ -5,12 +5,12 @@ def run(path):
     f = open(path)
     for linha in f:
         commands = linha.split('|')
+        print(float(commands[1]))
         subprocess.call("irsend SEND_ONCE LED_24_KEY " + commands[0], shell=True)
-        time.sleep(int(commands[1]))
+        time.sleep(float(commands[1]))
         
 def on():
     subprocess.call("irsend SEND_ONCE LED_24_KEY ON", shell=True)
     
 def off():
     subprocess.call("irsend SEND_ONCE LED_24_KEY OFF", shell=True)
-    
