81605e5b97f182d72b712ff98e565772c15823be
Joao Paulo Ataide <jpataide@gmail.com>
2017-12-01 11:26:20 -0300

    Alteracoes no log

---
+++ libs/log_util.py
@@ -0,0 +1,23 @@
+import time, logging, logging.handlers, os, datetime
+import envio_email
+
+def log_info(msg):
+    logging.info(time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()) + " - " + msg)
+
+def log_excecao(msg):
+    logging.exception(time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()) + ' - ' + msg)
+    envio_email.envia_email("Erro no sistema", msg)
+
+def init():
+    handler = logging.handlers.WatchedFileHandler(
+    os.environ.get("LOGFILE", "logs/LOG{}.log".format(time.strftime("_%Y_%m_%d"))))
+    formatter = logging.Formatter(logging.BASIC_FORMAT)
+    handler.setFormatter(formatter)
+    root = logging.getLogger()
+    root.setLevel(os.environ.get("LOGLEVEL", "INFO"))
+    root.addHandler(handler)
+
+init()
+log_info("teste")
+
+log_excecao("teste excessao")
